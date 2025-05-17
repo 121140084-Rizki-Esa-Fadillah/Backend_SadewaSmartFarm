031012053af9d6342b5eea630479646340f37dc6
RizkiEsaFadillah <rizkiifadillah456@gmail.com>
2025-05-17 23:09:15 +0700

    upload

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 
 try {
     if (!admin.apps.length) {
-        const serviceAccount = require("../firebase-admin.json");
+        const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG_JSON);
 
         admin.initializeApp({
             credential: admin.credential.cert(serviceAccount),
